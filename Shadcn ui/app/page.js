34da6b6b772d7cd7896b6a228a16991add90f472
Abdullah <itsabdullah14311211@gmail.com>
2024-07-04 10:43:07 +0500

    Add files via upload

---
+++ Shadcn ui/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Btn from "./Components/Btn";
+
+export default function Home() {
+  return (
+    <>
+    <h1>hellow</h1>
+    <Btn></Btn>
+    </>
+  
+  );
+}
